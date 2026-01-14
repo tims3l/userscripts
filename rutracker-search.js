90217f4bb87a60d28db7c4b918686c30d1b6f52d
tims3l <korody.istvan+tims3l@gmail.com>
2026-01-14 18:29:15 +0100

    rutracker search match & title fix

--- rutracker-search.js
+++ rutracker-search.js
@@ -4,12 +4,12 @@
 // @version      2026-01-14
 // @description  rewrites the document name to the search text
 // @author       tims3l
-// @match        https://rutracker.org/forum/tracker.php?nm=*
+// @match        https://rutracker.org/forum/tracker.php*nm=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=rutracker.org
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    document.title = 'ru "' + document.getElementById('search-text').value + '"';
+    document.title = document.getElementById('search-text').value;
 })();
